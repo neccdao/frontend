fc0cbf95952acb6b1c0919aecca13062f5e9a603
Eric Juta <ericjohnjuta@gmail.com>
2021-11-23 16:04:03 +0000

    feat(vite.config.js): vite-plugin-sentry

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import viteSentry from "vite-plugin-sentry";
+
+const sentryConfig = {
+  authToken: "0015463a69284d88ac6c762ce40bf9bab595c1b7abb14f0db7ae479199869d88",
+  org: "necc-protocol",
+  project: "frontend",
+  deploy: {
+    env: "production",
+  },
+  setCommits: {
+    auto: true,
+  },
+  sourceMaps: {
+    include: ["./dist/assets"],
+    ignore: ["node_modules"],
+    urlPrefix: "~/assets",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh(), viteSentry(sentryConfig)],
+});
